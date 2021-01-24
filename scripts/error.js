c58cb098d7ab2a60bcabadc82bb0bcd450bd2d99
StijnVandendriessche1 <stijn.vandendriessche3@student.howest.be>
2021-01-24 11:41:40 +0100

    small edits to 404

---
+++ scripts/error.js
@@ -0,0 +1,7 @@
+const init = function () 
+{
+    alert("You will be redirected to the login page");
+    window.open("index.html", "_self");
+}
+
+document.addEventListener('DOMContentLoaded', init);
